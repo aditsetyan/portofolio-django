93da05aa2632559635f9cc6888420dad67c5b37d
Adit Setya Nugroho <aditsetyan884@gmail.com>
2026-01-21 19:21:06 +0700

    fix missing pip

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
-# Install dependencies menggunakan Python 3.9
+# 1. Pastikan PIP terinstall di Python 3.9
+python3.9 -m ensurepip --default-pip
+
+# 2. Install dependencies (Django, dll)
 python3.9 -m pip install -r requirements.txt
 
-# Jalankan perintah collectstatic untuk mengumpulkan file statis
+# 3. Kumpulkan file statis
 python3.9 manage.py collectstatic --noinput --clear
